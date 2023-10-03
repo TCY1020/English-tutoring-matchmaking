62dff4baf9b67ec94f7535e1ef1e502c13d3a0ac
TCY1020 <a25023423@gmail.com>
2023-10-03 14:57:17 +0800

    feat:add user data

--- seeders/20230929042116-user.js
+++ seeders/20230929042116-user.js
@@ -1,5 +1,6 @@
 'use strict'
 const bcrypt = require('bcryptjs')
+const { faker } = require('@faker-js/faker')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -13,36 +14,56 @@ module.exports = {
     }, {
       email: 'user1@example.com',
       password: await bcrypt.hash('12345678', 10),
+      name: faker.person.fullName(),
       account: 'user1',
       role: 'student',
+      country: faker.location.county(),
+      introduction: faker.lorem.sentence({ min: 5, max: 15 }),
+      avatar: faker.image.avatar(),
       created_at: new Date(),
       updated_at: new Date()
     }, {
       email: 'user2@example.com',
       password: await bcrypt.hash('12345678', 10),
+      name: faker.person.fullName(),
       account: 'user2',
       role: 'student',
+      country: faker.location.county(),
+      introduction: faker.lorem.sentence({ min: 5, max: 15 }),
+      avatar: faker.image.avatar(),
       created_at: new Date(),
       updated_at: new Date()
     }, {
       email: 'user3@example.com',
       password: await bcrypt.hash('12345678', 10),
+      name: faker.person.fullName(),
       account: 'user3',
       role: 'student',
+      country: faker.location.county(),
+      introduction: faker.lorem.sentence({ min: 5, max: 15 }),
+      avatar: faker.image.avatar(),
       created_at: new Date(),
       updated_at: new Date()
     }, {
       email: 'user4@example.com',
       password: await bcrypt.hash('12345678', 10),
+      name: faker.person.fullName(),
       account: 'user4',
       role: 'teacher',
+      country: faker.location.county(),
+      teaching_style: faker.lorem.sentence({ min: 5, max: 15 }),
+      avatar: faker.image.avatar(),
       created_at: new Date(),
       updated_at: new Date()
     }, {
       email: 'user5@example.com',
       password: await bcrypt.hash('12345678', 10),
+      name: faker.person.fullName(),
       account: 'user5',
       role: 'teacher',
+      country: faker.location.county(),
+      teaching_style: faker.lorem.sentence({ min: 5, max: 15 }),
+      avatar: faker.image.avatar(),
       created_at: new Date(),
       updated_at: new Date()
     }])
